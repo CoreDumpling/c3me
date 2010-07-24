52fd2f500d01cd4aa5f5775395e85b83c4c4d313
Sima Qian <coredumpling@gmail.com>
2010-07-23 21:52:08 -0700

    Add BIC_PRINT_DWORD macro

--- bicdump.c
+++ bicdump.c
@@ -5,6 +5,11 @@
                 printf("BIC+%04x " #field ": %s\n", \
                        (int) &bic.field - (int) &bic, bic.field); \
         } while (0);
+#define BIC_PRINT_DWORD(bic, field, fmt) \
+        do { \
+                printf("BIC+%04x (" #field "): " fmt "\n", \
+                       (int) &bic.field - (int) &bic, bic.field); \
+        } while (0);
 #define BIC_PRINT_DWORD_BUF(bic, field, fmt) \
         do { \
                 int len = sizeof(bic.field) / sizeof(DWORD); \
@@ -27,6 +32,7 @@ int main(int argc, char** argv) {
 
         BIC_PRINT_STR(bic, BIC);
         BIC_PRINT_DWORD_BUF(bic, no_idea_1, "0x%08x");
+        BIC_PRINT_DWORD(bic, biqFilenamePtr, "0x%08x");
         /*
         for (i = 0; i < szBic; i += 4) {
                 printf("BIC+%04x: %x\n", i, *ptr);
