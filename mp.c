29f6d5078678d1f194c50098420afadaf3e80698
Sima Qian <coredumpling@gmail.com>
2011-04-12 19:42:58 -0700

    K&R style cleanup

--- mp.c
+++ mp.c
@@ -1,38 +1,42 @@
 #include "c3c.h"
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-        uint32_t mask = 0; // the bitmask for which players are human-controlled
-        char flag = 1; // the flag for whether or not multiplayer is enabled
+int main(int argc, char **argv)
+{
+    /* bitmask for which players are human-controlled */
+    uint32_t mask = 0;
+    /* flag for whether or not multiplayer is enabled */
+    char flag = 1;
 
-        if (!WriteC3CMemory(MP_FLAG_ADDR, &flag, sizeof(char))) {
-		fprintf(stderr, "Unable to activate multiplayer mode.\n");
-		return 1;
+    if (!WriteC3CMemory(MP_FLAG_ADDR, &flag, sizeof(char))) {
+	fprintf(stderr, "Unable to activate multiplayer mode.\n");
+	return 1;
+    }
+
+    mask = 0;
+    int a;
+    for (a = 1; a < argc; ++a) {
+	int p = atoi(argv[a]);
+	if (p < 32) {
+	    mask |= 1 << p;
+	} else {
+	    printf("Invalid player: %d\n", p);
 	}
+    }
 
-        mask = 0;
-	int a;
-        for (a = 1; a < argc; ++a) {
-                int p = atoi(argv[a]);
-                if (p < 32) {
-                        mask |= 1 << p;
-                } else {
-                        printf("Invalid player: %d\n", p);
-                }
-        }
+    if (!WriteC3CMemory(PLAYER_MASK_ADDR, &mask, sizeof(uint32_t)))
+	return 1;
+    if (!ReadC3CMemory(PLAYER_MASK_ADDR, &mask, sizeof(uint32_t)))
+	return 1;
 
-        if (!WriteC3CMemory(PLAYER_MASK_ADDR, &mask, sizeof(uint32_t)))
-		return 1;
-        if (!ReadC3CMemory(PLAYER_MASK_ADDR, &mask, sizeof(uint32_t))) return 1;
+    printf("Current human players:");
+    int p;
+    for (p = 0; p < 32; ++p) {
+	if (1 << p & mask) {
+	    printf(" %d", p);
+	}
+    }
+    printf("\n");
 
-        printf("Current human players:");
-	int p;
-        for (p = 0; p < 32; ++p) {
-                if (1 << p & mask) {
-                        printf(" %d", p);
-                }
-        }
-        printf("\n");
-
-        return 0;
+    return 0;
 }
